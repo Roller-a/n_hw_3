328929ae27041cee8f53451263dfa59779db91d3
Raller <ms.mashanov@mail.ru>
2024-04-04 12:38:57 +0300

    Final commit

--- src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
@@ -8,18 +8,19 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 import com.codeborne.selenide.Condition;
 import org.openqa.selenium.Keys;
 
 public class AppCardDeliveryTest {
 
-    private String generateDate (int addDays, String pattern) {
+    private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
     public void shouldBeSuccessfullyCompleted() {
-        open ("http://localhost:9999");
+        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Якутск");
         String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
